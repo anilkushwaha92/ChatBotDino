a18f17cc375449b1bd5a165c91096dfab4790f70
Anil Kushwaha <anil@appypiellp.com>
2021-05-14 17:56:47 +0530

    Add all file in the framework

--- Example/Pods/Target Support Files/Pods-ChatBotDino_Example/Pods-ChatBotDino_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ChatBotDino_Example/Pods-ChatBotDino_Example-frameworks.sh
@@ -175,10 +175,12 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/ChatBotDino/ChatBotDino.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AMKeyboardFrameTracker/AMKeyboardFrameTracker.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PaddingLabel/PaddingLabel.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/ChatBotDino/ChatBotDino.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AMKeyboardFrameTracker/AMKeyboardFrameTracker.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PaddingLabel/PaddingLabel.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
